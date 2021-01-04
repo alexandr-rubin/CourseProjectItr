182f9a32bb31c510588166b3454b7fe7dbfa61c1
Rubin Alexandr <alexandrrubin0715@gmail.com>
2021-01-04 13:56:35 +0300

    Add project files.

---
+++ CourseProjectItr/Areas/Identity/Data/CourseDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProjectItr.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseProjectItr.Data
+{
+    public class CourseDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public CourseDbContext(DbContextOptions<CourseDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ CourseProjectItr/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CourseProjectItr.Areas.Identity.Data;
+using CourseProjectItr.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CourseProjectItr.Areas.Identity.IdentityHostingStartup))]
+namespace CourseProjectItr.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CourseDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CourseDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CourseDbContext>();
+            });
+        }
+    }
+}
