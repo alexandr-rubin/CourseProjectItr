883647e2cb53c3f76219526d681fb2de7fcc5d4e
Rubin Alexandr <alexandrrubin0715@gmail.com>
2021-01-04 16:26:41 +0300

    Add login and register

--- CourseProjectItr/Areas/Identity/Data/ApplicationUser.cs
+++ CourseProjectItr/Areas/Identity/Data/ApplicationUser.cs
@@ -9,5 +9,10 @@ namespace CourseProjectItr.Areas.Identity.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        public string LastName { get; set; }
     }
 }
